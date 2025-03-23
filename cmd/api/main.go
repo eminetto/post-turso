454551eed5cb9a9a696b6c5ec5a32c8836a24c3e
Elton Minetto <elton@minetto.dev>
2025-03-23 17:55:18 -0300

    feat: add gracefull shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,8 +8,12 @@ import (
 	"github.com/eminetto/post-turso/config"
 	"github.com/eminetto/post-turso/internal/http/chi"
 	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+const TIMEOUT = 30 * time.Second
 
 func main() {
 	cfg, err := config.GetConfig()
@@ -17,7 +21,11 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	ctx := context.Background()
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT,
+	)
+	defer stop()
 	repo, err := turso.NewRepository(cfg.DBName, cfg.TursoDatabaseURL, cfg.TursoAuthToken)
 	if err != nil {
 		fmt.Println(err)
@@ -33,10 +41,36 @@ func main() {
 		Addr:         ":" + cfg.Port,
 		Handler:      http.DefaultServeMux,
 	}
+
+	errShutdown := make(chan error, 1)
+	go shutdown(srv, ctx, errShutdown)
+	fmt.Printf("Listening on port %s\n", cfg.Port)
 	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		fmt.Println(err)
+		return
+	}
+	err = <-errShutdown
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+}
 
+func shutdown(server *http.Server, ctxShutdown context.Context, errShutdown chan error) {
+	<-ctxShutdown.Done()
+
+	ctxTimeout, stop := context.WithTimeout(context.Background(), TIMEOUT)
+	defer stop()
+
+	err := server.Shutdown(ctxTimeout)
+	switch err {
+	case nil:
+		fmt.Printf("\nShutting down server...\n")
+		errShutdown <- nil
+	case context.DeadlineExceeded:
+		errShutdown <- fmt.Errorf("Forcing closing the server")
+	default:
+		errShutdown <- fmt.Errorf("Forcing closing the server")
+	}
 }
